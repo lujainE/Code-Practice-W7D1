b7e5056d38339a7588630e08ae6d5bd3fb096d1a
lujainE <lujain.z.e@hotmail.com>
2022-10-17 09:17:38 +0300

    message

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#FFC18E';
+		header.style.backgroundColor = '#b8d9a5';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
